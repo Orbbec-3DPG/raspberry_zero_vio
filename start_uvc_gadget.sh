5391c272c613e07ba0310a26461a7f3bfa8a57c3
547135457@qq.com <you@example.com>
2018-06-10 19:20:40 +0000

    fix start uvc gadget

--- start_uvc_gadget.sh
+++ start_uvc_gadget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 2s
-/home/pi/vio/raspberry_zero_vio/config_imu.sh
+
 /home/pi/vio/raspberry_zero_vio/config_uvc.sh
 sleep 2s
-/home/pi/vio/raspberry_zero_vio/uvc-gadget/uvc-gadget -u /dev/video1 -v /dev/video0 -w
+#/home/pi/vio/raspberry_zero_vio/uvc-gadget/uvc-gadget -u /dev/video1 -v /dev/video0 -w
