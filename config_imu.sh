4a88ff17785d79da6ab9a5631c2f2d399b8d4ffc
547135457@qq.com <you@example.com>
2018-06-10 16:18:43 +0000

    fix config imu

--- config_imu.sh
+++ config_imu.sh
@@ -1,6 +1,6 @@
-
-export ACCLE_DEV = iio:device0
-export GYRO_DEV  = iio:device1
+#!/bin/bash
+export ACCLE_DEV=iio:device0
+export GYRO_DEV=iio:device1
 
 #disable the buffer
 echo 0 > /sys/bus/iio/devices/${ACCLE_DEV}/buffer/enable
@@ -18,9 +18,9 @@ echo 208 > /sys/bus/iio/devices/${ACCLE_DEV}/sampling_frequency
 
 #config dev
 #enable channel
-echo 1 > /sys/bus/iio/devices/${GYRO_DEV}/scan_elements/in_gyro_x_en
-echo 1 > /sys/bus/iio/devices/${GYRO_DEV}/scan_elements/in_gyro_y_en
-echo 1 > /sys/bus/iio/devices/${GYRO_DEV}/scan_elements/in_gyro_z_en
+echo 1 > /sys/bus/iio/devices/${GYRO_DEV}/scan_elements/in_anglvel_x_en
+echo 1 > /sys/bus/iio/devices/${GYRO_DEV}/scan_elements/in_anglvel_y_en
+echo 1 > /sys/bus/iio/devices/${GYRO_DEV}/scan_elements/in_anglvel_z_en
 echo 1 > /sys/bus/iio/devices/${GYRO_DEV}/scan_elements/in_timestamp_en
 #set freq
 echo 208 > /sys/bus/iio/devices/${GYRO_DEV}/sampling_frequency
